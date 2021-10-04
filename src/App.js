da8bf857d25f027fe55af13b78ad49d0fecdd31e
burkero2 <ronanburke@codeinstitute.net>
2021-10-04 08:09:18 +0000

    Context added

--- src/App.js
+++ src/App.js
@@ -8,26 +8,30 @@ import SignInForm from './pages/auth/SignInForm'
 import SignUpForm from './pages/auth/SignUpForm'
 
 
+
+
 function App() {
+
   return (
-    <div className={styles.App}>
-      <NavBar />
-      <Container className={styles.Main}>
-        <Switch>
-          <Route exact path="/" render={() => 
-            <h1>Home page</h1>
-          } />
-          <Route exact path="/signin" render={() => 
-            <SignInForm />
-          } />
-          <Route exact path="/signup" render={() => 
-            <SignUpForm />
-          } />
-          <Route render={() => <p>Page not found!</p>} /> 
-        </Switch>
-      </Container>
-    </div>
+        <div className={styles.App}>
+          <NavBar />
+          <Container className={styles.Main}>
+            <Switch>
+              <Route exact path="/" render={() => 
+                <h1>Home page</h1>
+              } />
+              <Route exact path="/signin" render={() => 
+                <SignInForm />
+              } />
+              <Route exact path="/signup" render={() => 
+                <SignUpForm />
+              } />
+              <Route render={() => <p>Page not found!</p>} /> 
+            </Switch>
+          </Container>
+        </div>
   );
-}
+}   
+
 
 export default App;
--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import axios from "axios";
-
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
 import Button from "react-bootstrap/Button";
@@ -8,12 +7,11 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
-
 import { Link, useHistory } from "react-router-dom";
-
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
+import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 
 function SignInForm() {
 //   Add your component logic here
@@ -26,21 +24,29 @@ const {username, password} = signInData;
 const history = useHistory();
 const [errors, setErrors] = useState({});
 
-const handleChange = (event) => {
-    setSignInData({
-        ...signInData,
-        [event.target.name]:event.target.value
-    });
-};
+const setCurrentUser = useSetCurrentUser();
+
+
+
+
 
 const handleSubmit = async (event) => {
     event.preventDefault();
     try{
-        await axios.post("/dj-rest-auth/login/", signInData);
-        history.push("");
+      const { data } = await axios.post("/dj-rest-auth/login/", signInData);
+      console.log("signup data", data);
+      setCurrentUser(data.user);
+      history.push("/");
     } catch(err){
         setErrors(err.response?.data)
     }
+};
+
+const handleChange = (event) => {
+  setSignInData({
+      ...signInData,
+      [event.target.name]:event.target.value
+  });
 };
 
   return (
